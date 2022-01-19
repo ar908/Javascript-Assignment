6a08a5e1821ec1a1b7c0ed24baeb0d05725e47ab
ar908 <96685835+ar908@users.noreply.github.com>
2022-01-19 23:16:12 +0530

    fav_movie sub

--- assignments/favourite_movie/favourite_movie.js
+++ assignments/favourite_movie/favourite_movie.js
@@ -2,6 +2,13 @@ const movies = [];
 
 function favouriteMovie(operation, movie) {
     // Write your code here
+    if(operation==='add' ){
+        movies.push(movie);
+
+    }else if(operation==='remove'){
+        movies.pop();
+    }
+    return movies;
 }
 
 module.exports = favouriteMovie;
