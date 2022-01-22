f7e295c167d667432a81dd9bfbd2190fe9219ae0
ar908 <96685835+ar908@users.noreply.github.com>
2022-01-22 11:43:29 +0530

    playWithArray sub

--- assignments/play_with_array/play_with_array.js
+++ assignments/play_with_array/play_with_array.js
@@ -7,7 +7,13 @@ function getEven(arr) {
 
         Write you code below
     */
-
+   var arr1 = []
+   for(var i=0; i<arr.length; i++){
+       if(arr[i]%2==0){
+           arr1.push(arr[i]);
+       }
+   }
+return arr1 
 }
 
 function multiplyByN(arr, n) {
@@ -18,6 +24,12 @@ function multiplyByN(arr, n) {
             Output: [3,9,13,165]
         Write you code below
     */
+   
+   for(var i=0;i<arr.length; i++){
+       arr[i]=arr[i]*n;
+
+   }
+return arr;
 }
 
 function removeNthElement(arr, n) {
@@ -28,6 +40,8 @@ function removeNthElement(arr, n) {
             Output: [1,3,4,7]
         Write you code below
     */
+        arr.splice(n, 1)
+       return arr 
 }
 
 module.exports = {
