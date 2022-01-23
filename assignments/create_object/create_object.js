029e82fef28cbdceff89c12dba9445718e5fafc6
ar908 <96685835+ar908@users.noreply.github.com>
2022-01-23 11:24:47 +0530

    creatout_sub

--- assignments/create_object/create_object.js
+++ assignments/create_object/create_object.js
@@ -1,5 +1,11 @@
 function CreateObject(arr) {
     // Write your code here
+    var temp = {};
+    for(var i = 0; i<arr.length; i+=2){
+        temp[arr[i]]=arr[i+1];
+
+    }
+    return temp;
 }
 
 module.exports = CreateObject;
